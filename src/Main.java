cddd7c45ce9bf5f46f72152830628dae0a62832d
Devansh Sanghvi <dsanghvi@truefit.com>
2019-07-30 11:28:49 +0530

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,11 @@ public class Main {
     public static void main(String[] args) throws IOException, JSONException {
         JSONArray json = readJsonAPI("https://api.jsonbin.io/b/5d31a1c4536bb970455172ca/latest");
         ArrayList<Product> products = deserelizeJsonInput(json);
-        if (args.length != 0) {
+        if (args.length == 0) {
             System.out.println("Incorrect Argument");
         } else {
             for (Product p : products) {
-                Promotions promo = new PromotionFactory(p).getPromotion("promotionSetB");
+                Promotions promo = new PromotionFactory(p).getPromotion(args[0]);
                 ProductDiscount pd = promo.applyPromotion();
                 String jpd = convertToJson(pd);
                 System.out.println(jpd);
